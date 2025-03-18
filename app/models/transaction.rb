b34aa78975befa8af2be34a29b8a7e82c064f984
Davi Mendes <davi26030@gmail.com>
2025-03-18 18:21:33 -0300

    feat: add cpf and value validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :transaction_type
 
-  validates :date, :value, :cpf, :card, :store_owner, :store_name, presence: true
-  validates :value, numericality: true
+  validates :date, :value, :cpf, :card, :time, :store_owner, :store_name, presence: true
+  validates :cpf, format: { with: /\A\d{11}\z/, message: "deve ter 11 dígitos" }
+  validates :value, numericality: { greater_than: 0 }
 end
