2da52bf09c3c030db0cc51916b2d90a73573f8d4
Davi Mendes <davi.mendes@castgroup.com.br>
2025-03-19 19:17:12 -0300

    feat: add text color based on income

--- app/models/transaction_type.rb
+++ app/models/transaction_type.rb
@@ -2,4 +2,12 @@ class TransactionType < ApplicationRecord
   has_many :transactions
 
   validates :code, :description, :nature, :sign, presence: true
+
+  def income?
+    sign == '+'
+  end
+
+  def expense?
+    sign == '-'
+  end
 end
