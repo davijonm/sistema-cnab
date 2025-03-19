1cfc1760c2eda5113e5dc3e43793b15804e0aecd
Davi Mendes <davi.mendes@castgroup.com.br>
2025-03-19 18:22:13 -0300

    refactor: remove unused files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root "uploads#index"
-  resources :uploads, only: [:new, :create, :index]
+  resources :uploads, only: [:create, :index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
