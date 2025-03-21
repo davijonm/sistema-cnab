05bec746db303731af58d8e512ec4aa094639bbd
Davi Mendes <davi.mendes@castgroup.com.br>
2025-03-21 18:59:36 -0300

    feat: create controller for charts and add js config

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,6 +2,15 @@ class ChartsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  end
+    # transacoes por mês
+    @transactions_by_month = Transaction.group_by_month(:date).sum(:value)
 
+    # total por loja
+    @transactions_by_store = Transaction.group(:store_name).sum(:value)
+
+    # tipo de transacao
+    @transactions_by_type = Transaction.joins(:transaction_type)
+                                       .group('transaction_types.description')
+                                       .sum(:value)
+  end
 end
